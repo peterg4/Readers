9a897bcb8d689716648e1421c0341c75b2c2c046
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-07-05 11:22:52 -0400

    navbar scrolly

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  @ViewChild('navbar') nav: ElementRef;
+
+  ngAfterViewInit() {
+    console.log(this.nav.nativeElement.innerHTML);
+  }
+  @HostListener('document:scroll', ['$event']) onScrollEvent($event){
+    console.log(this.nav)
+    console.log($event);
+    console.log("scrolling");
+    window.onscroll = () => { 
+      "use strict";
+      if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20 ) {
+        this.nav.nativeElement.classList.add("scrolled-nav");
+      } 
+      else {
+        this.nav.nativeElement.classList.remove("scrolled-nav");
+      }
+    } 
+  }
 }
 document.addEventListener('DOMContentLoaded', function() {
   let elems = document.querySelectorAll('.sidenav');
@@ -14,3 +35,17 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   var instances = M.Sidenav.init(elems, options);
 });
+
+
+/*
+var nav = document.getElementsByClassName("nav-wrapper");
+console.log(nav);
+window.onscroll = function () { 
+  "use strict";
+  if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20 ) {
+    nav.classList.remove("scolled-nav");
+  } 
+  else {
+    nav.classList.add("scolled-nav");
+  }
+};*/
